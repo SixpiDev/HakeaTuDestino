5db0fe37ca7ad9e05a2d6448d1c2d90b7ecd65d8
Victor <vcv183@gmail.com>
2017-11-25 17:10:14 +0100

    inicios

--- Daninimas/hibuddy/main.cpp
+++ Daninimas/hibuddy/main.cpp
@@ -2,7 +2,25 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(1000, 600), "hibuddyAPP");
+
+    sf::Texture tMap;
+    if (!tMap.loadFromFile("assets/mapa.png"))
+    {
+        // error...
+    }
+    sf::Sprite sMap;
+    sMap.setTexture(tMap);
+
+    sf::Texture tHud;
+    if (!tHud.loadFromFile("assets/hud.png"))
+    {
+        // error...
+    }
+    sf::Sprite sHud;
+    sHud.setTexture(tHud);
+
+
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
@@ -11,12 +29,36 @@ int main()
         sf::Event event;
         while (window.pollEvent(event))
         {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+            {
+                // move left...
+                sMap.move(-8,0);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+            {
+                // move right...
+                sMap.move(8,0);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+            {
+                // move up...
+                sMap.move(0,8);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+            {
+                // move left...
+                sMap.move(0,-8);
+            }
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::)){
+                sf::
+            }
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(sMap);
+        window.draw(sHud);
         window.display();
     }
 
